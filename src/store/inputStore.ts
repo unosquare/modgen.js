a7af37b1bca66c044733894282bba11087309d5a
AllierByron <99301366+AllierByron@users.noreply.github.com>
2023-09-14 12:33:22 -0600

    UI rework. New areas created for the user to provide examples and store them (#8)
    * UI rework. New areas created for the user to provide examples and store them
    
    * Refactoring, and new UI boxes for examples
    
    * Tag closing
    
    * Useless code removed
    
    * Logs, comments removed
    
    * Refactored some code
    
    ---------
    
    Co-authored-by: Byron <212220367@soy.utj.edu.mx>

--- src/store/inputStore.ts
+++ src/store/inputStore.ts
@@ -1,14 +1,44 @@
 import {create} from 'zustand';
 
-interface inputStore{
-    userInputs: string,
-    updateUserInput: (input:string) => void
+interface InputStore{
+    userInputs: string;
+    userResults:string;
+    updateUserInput: (input:string) => void;
+    updateUserResults: (input:string) => void;
 }
 
-export const userInputStore = create<inputStore>((set)=>
+export const userInputStore = create<InputStore>((set)=>
 ({
     userInputs: "",
+    userResults: "",
     updateUserInput:(input: string) => set(()=>({
         userInputs: input
+    })),
+    updateUserResults:(result: string) => set(()=>({
+        userResults: result
     }))
+}))
+
+export interface ShotsTypes {
+    input: string, 
+    result: string
+}
+
+interface ArrInterface{
+    examples: ShotsTypes[];
+    updateUserInputArr: (input:ShotsTypes)=>void;
+    deleteUserInputArr: ()=>void;
+}
+
+//store for array that stores user's input and results.
+//these are the examples that are fed to GPT. 
+export const userInputsArr = create<ArrInterface>((set)=>({
+    examples: [],
+    updateUserInputArr: (input:ShotsTypes)=> set((status:ArrInterface)=>({
+        examples:[...status.examples, {input: input.input, result: input.result}]
+    })),
+    deleteUserInputArr:() => set((status:any)=>{
+        status.examples = [];
+        return status.examples;
+    })    
 }))
