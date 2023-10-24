8d7dac9522178648a5c775170b775ce6c8c5263e
Byron <212220367@soy.utj.edu.mx>
2023-10-24 17:06:35 -0600

    prompt correction

--- src/app/promptTemplates.ts
+++ src/app/promptTemplates.ts
@@ -1,21 +1,21 @@
 import { ShotsTypes } from '@/store/inputStore';
 
 const masterPrompt = (input:string):string => {
-    return `You are a code generation machine 
-    you do not return anything other than code.
-    Based on the provided examples, you must 
-    provide a result based on the provided
-    input. If the provided input does
-    not contain code, send an error message 
-    between double quotation marks. Do not include
-    any extra code that is not asked for. Try 
-    to be as consistent as possible and follow 
-    the conventions used in the provided code.
-
-    input: '${input}'`;
+    return `You do not return anything other than code. 
+    You must be as consistent as 
+    possible and follow the conventions used in 
+    the provided examples and input.
+    If the provided input does not contain code, 
+    return an error message between double quotation
+    marks. Do not include any extra code that is 
+    not asked for. 
+    
+    input: 
+    ${input}`;
 };
 
 export function insertExamples(examples:ShotsTypes[], lastPrompt:string){
+    console.log(examples);
     const msjs:{"role":string, "content":string}[] = [];
     msjs.push({"role": "system", "content":"You are a code generation machine"});
     for (let i = 0; i < examples.length; i++) {
