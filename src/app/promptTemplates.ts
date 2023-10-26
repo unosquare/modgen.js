cc74abf97520580dd95ba8a83375f8e04eb0de8a
Byron <212220367@soy.utj.edu.mx>
2023-10-26 11:53:24 -0600

    Prompt improved

--- src/app/promptTemplates.ts
+++ src/app/promptTemplates.ts
@@ -1,23 +1,19 @@
 import { ShotsTypes } from '@/store/inputStore';
 
-const masterPrompt = (input:string):string => {
-    return `You do not return anything other than code. 
-    You must be as consistent as 
-    possible and follow the conventions used in 
-    the provided examples and input.
-    If the provided input does not contain code, 
-    return an error message between double quotation
-    marks. Do not include any extra code that is 
-    not asked for. 
-    
-    input: 
-    ${input}`;
+const masterPrompt = (input?:string):string => {
+    return `You are a code generation machine, and 
+    you do not return anything other than code. You must be as consistent as 
+    possible and follow the conventions used in the provided examples, pay close
+    attention to the assistant responses examples and try to adapt your response
+    to them.
+    If the provided last input does not contain code return an error message 
+    between double quotation marks. Do not include any extra code that is 
+    not asked for.`;
 };
 
 export function insertExamples(examples:ShotsTypes[], lastPrompt:string){
-    console.log(examples);
     const msjs:{"role":string, "content":string}[] = [];
-    msjs.push({"role": "system", "content":"You are a code generation machine"});
+    msjs.push({"role": "system", "content":masterPrompt()});
     for (let i = 0; i < examples.length; i++) {
         const element = examples[i];        
         msjs.push(
@@ -26,11 +22,12 @@ export function insertExamples(examples:ShotsTypes[], lastPrompt:string){
         );
         if(examples.length  === (i+1)){
             msjs.push(
-                {"role":"user", "content": masterPrompt(lastPrompt)}
+                {"role":"user", "content": lastPrompt}
             );
             break;
         }
     }
+    console.log(msjs)
     return msjs;
 }
 
