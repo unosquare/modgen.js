94bf99d68d73091120c9cd91070942ce9521979a
Byron <212220367@soy.utj.edu.mx>
2023-11-07 11:07:46 -0600

    Resizing issue fix n3

--- src/app/components/Container.tsx
+++ src/app/components/Container.tsx
@@ -5,7 +5,7 @@ interface ContainerTypes {
 
 const Container = ({ children, className }: ContainerTypes) => {
     const classesName = `${className} flex-auto flex flex-col flex-initial 
-                                    bg-gray-400 px-5 py-3 h-3/4
+                                    bg-gray-400 px-5 py-3 h-5/6
                                     rounded-lg bg-opacity-20 
                                     backdrop-blur-lg
                                     hover:shadow-lg hover:shadow-blue-950`;
